0fc79a11d8c54394f0a9112a0b9d6f5f7f194ea1
Akshay Arora <akshay575@yahoo.com>
2020-06-17 02:07:26 +0530

    update server, add redis config and express routes

--- server/index.js
+++ server/index.js
@@ -24,3 +24,50 @@ pgClient.on('error', () => console.log('Lost PG Connection!'));
 pgClient
     .query('CREATE TABLE IF NOT EXISTS values (number INT)')
     .catch(err => console.log(err));
+
+// Redis Client Setup
+const redis = require('redis');
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+// duplicate connection, as one client can either publish or subscribe information at a time
+const redisPublisher = redisClient.duplicate();
+
+// Express Route Handlers
+app.get('/', (req, res) => {
+    res.send('Hi!');
+});
+
+app.get('/values/all', async (req, res) => {
+    const values = await pgClient.query('SELECT * FROM values');
+    res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+    redisClient.hgetall('values', (err, values) => {
+        res.send(values);
+    });
+});
+
+app.post('/values', async (req, res) => {
+    const index = req.body.index;
+
+    if(parseInt(index) > 40) {
+        return res.status(422).send('Index too high!');
+    }
+
+    // storing a new index in redis
+    redisClient.hset('values', index, 'Not calculated yet!');
+    // publish an insert event so that worker process can listen and start calculating the value
+    redisPublisher.publish('insert', index);
+    // add index to table
+    pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+    res.send({ working: true });
+})
+
+app.listen(5000, err => {
+    console.log('Listening on port 5000!')
+});
